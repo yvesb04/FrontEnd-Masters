45e9f16534c480b096bc2488a1d99df8014d8ec9
Yves Bonneau <yves.bonneau2004@gmail.com>
2023-06-29 15:40:38 +0100

    FULLY WORKING WORDLE

--- Beginner/Wordle/wordle.js
+++ Beginner/Wordle/wordle.js
@@ -5,6 +5,7 @@ const letters = document.querySelectorAll(".game-word");
 async function init() {
   let rowNumber = 0;
   let currentWord = "";
+  let gameOver = false;
 
   const wordAPI = await fetch(
     "https://words.dev-apis.com/word-of-the-day?random=1"
@@ -15,18 +16,22 @@ async function init() {
   console.log(wordOfDay);
 
   document.addEventListener("keyup", function (event) {
+    if (gameOver) {
+      return;
+    }
+
     const letter = event.key;
     if (letter === "Enter") {
-      handleEnter(letter);
+      handleEnter();
     } else if (letter === "Backspace") {
-      handleBackSpace(letter);
+      handleBackSpace();
     } else if (isLetter(letter) == true) {
       handleLetter(letter);
     }
     console.log(currentWord);
   });
 
-  async function handleEnter(enter) {
+  async function handleEnter() {
     if (currentWord.length === 5) {
       // TODO
       const validateWord = await fetch(
@@ -39,7 +44,20 @@ async function init() {
       const validation = await validateWord.json();
       wordIsValid = validation.validWord;
 
-        //Check first for correct
+      if (!wordIsValid) {
+        for (let i = 0; i < 5; i++) {
+          letterNow = letters[rowNumber * ANSWER_LENGTH + i];
+          letterNow.classList.remove("invalid-letter");
+          setTimeout(function () {
+            letters[rowNumber * ANSWER_LENGTH + i].classList.add(
+              "invalid-letter"
+            );
+          }, 10);
+        }
+        return;
+      }
+
+      //Check first for correct
       for (let i = 0; i < 5; i++) {
         if (currentWord[i] == wordOfDay[i]) {
           letters[rowNumber * ANSWER_LENGTH + i].classList.add(
@@ -48,15 +66,42 @@ async function init() {
         }
       }
 
-
-
       if (currentWord === wordOfDay) {
         alert("YOU WON");
-      } else if (wordIsValid) {
+        gameOver = true;
+      } else {
         //Check if word exists and identify words colors
+        const letterMap = new Map();
+        for (const letter of wordOfDay) {
+          letterMap.set(letter, (letterMap.get(letter) || 0) + 1);
+        }
+
+        for (let i = 0; i < 5; i++) {
+          if (currentWord[i] == wordOfDay[i]) {
+            letterMap.set(currentWord[i], letterMap.get(currentWord[i]) - 1);
+          } else if (
+            letterMap.has(currentWord[i]) &&
+            letterMap.get(currentWord[i]) > 0
+          ) {
+            letters[rowNumber * ANSWER_LENGTH + i].classList.add(
+              "letter-present"
+            );
+            letterMap.set(currentWord[i], letterMap.get(currentWord[i]) - 1);
+          } else {
+            letters[rowNumber * ANSWER_LENGTH + i].classList.add(
+              "letter-wrong"
+            );
+          }
+        }
+
         //Go next line
         rowNumber++;
         currentWord = "";
+      }
+
+      if (rowNumber === NUM_ANSWERS) {
+        gameOver = true;
+        alert("GAME FUCKING OVER BRO, BETTER LUCK NEXT TIME :P");
       }
     }
   }
